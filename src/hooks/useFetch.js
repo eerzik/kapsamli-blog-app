1b6e75697769d9b2b36fe976a62043a599595f4d
Engin ERZİK <erzik.engin@gmail.com>
2023-06-15 14:49:59 +0300

    Veri Eklemek İçin HOOK Oluşturma

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,33 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (url, method = "GET") => {
+
     const [data, setData] = useState(null);
     const [hata, setHata] = useState(null);
     const [yukleniyor, setYukleniyor] = useState(false);
+
+    const [options, setOptions] = useState(null)
+    const postData = (data) => {
+        setOptions(
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify
+            }
+        )
+    }
 
     useEffect(() => {
 
         //Veri çekmenin iptal edildiğini söylemek için
         const controller = new AbortController();
-        const fetchData = async () => {
+        const fetchData = async (fetchOptions) => {
             setYukleniyor(true);
 
             try {
-                const res = await fetch(url, { signal: controller.signal })
+                const res = await fetch(url, { ...fetchOptions, signal: controller.signal })
                 if (!res.ok) {
                     throw new Error(res.status);
                 }
@@ -23,7 +37,7 @@ export const useFetch = (url) => {
                 setHata(null);
             } catch (error) {
                 if (error.name === "AbortError") {
-                    console.log('Vari Çekme İptal Edildi')
+                    console.log('Veri Çekme İptal Edildi')
                 } else {
                     setYukleniyor(false)
                     setHata('Veriler Yüklenemedi!')
@@ -31,12 +45,21 @@ export const useFetch = (url) => {
             }
 
         }
-        fetchData();
+
+        if (method === "GET") {
+            fetchData();
+        }
+
+        if (method === "POST" && options) {
+            fetchData(options)
+        }
+
+
         return () => {
             controller.abort();
         }
 
-    }, [url]);
+    }, [url,method,options]);
 
-    return { data, yukleniyor, hata }
+    return { data, yukleniyor, hata, postData }
 }
