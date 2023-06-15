09bcc9182a4f8068afd3c03c53ce9b69abc2115d
Engin ERZİK <erzik.engin@gmail.com>
2023-06-15 17:16:06 +0300

    Context Verilerine Erişme İşlemi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import Searchbar from './Searchbar'
+import { useTheme } from '../hooks/useTheme'
+
 export default function Navbar() {
+
+    const {bgColor}=useTheme();
+
     return (
-        <div className='navbar' >
+        //Navbar ın rengini themecontext den getirdik.
+        <div className='navbar' style={{backgroundColor:bgColor}} >
             <nav>
                 <Link to='/' className="brand" > <h1>AOS Blog</h1> </Link>
                 <Searchbar />
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const ThemeContext = createContext();
 export function ThemeProvider({ children }) {
     return (
-        <ThemeContext.Provider>
+        <ThemeContext.Provider value={{ bgColor: '#778beb' }}>
             {children}
         </ThemeContext.Provider>
     )
