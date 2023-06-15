1a700eb9c9329800a64a2311f980db5c141ed3fa
Engin ERZİK <erzik.engin@gmail.com>
2023-06-15 14:37:46 +0300

    Yazı Kategorilerini Ekleme İşlemi

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,15 +1,29 @@
 import './Create.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export default function Create() {
 
     const [baslik, setBaslik] = useState('')
     const [icerik, setIcerik] = useState('')
     const [okunmaSuresi, setOkunmaSuresi] = useState('')
+    const [yeniKategori, setYeniKategori] = useState('')
+    const [kategoriler, setKategoriler] = useState([])
+    const kategoriInput = useRef(null)
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(baslik, icerik, okunmaSuresi);
+        console.log(baslik, icerik, okunmaSuresi, kategoriler);
+    }
+
+    const handleAdd = (e) => {
+        e.preventDefault();
+        const yKat = yeniKategori.trim()
+        if (yKat && !kategoriler.includes(yKat)) {
+            setKategoriler(oKat => [...oKat, yeniKategori])
+        }
+
+        setYeniKategori('')
+        kategoriInput.current.focus()
     }
 
     return (
@@ -18,16 +32,25 @@ export default function Create() {
             <form onSubmit={handleSubmit}>
                 <label>
                     <span>Yazı Başlık</span>
-                    <input type="text" onChange={(e)=>setBaslik(e.target.value)} value={baslik} required />
+                    <input type="text" onChange={(e) => setBaslik(e.target.value)} value={baslik} required />
                 </label>
                 {/* kategoriler gelecek */}
                 <label>
+                    <span>Yazı Kategorileri : </span>
+                    <div className='categories'>
+                        <input type='text' onChange={(e) => setYeniKategori(e.target.value)} value={yeniKategori} ref={kategoriInput} />
+                        <button onClick={handleAdd} className='btnAdd btn' >Ekle</button>
+                    </div>
+                </label>
+                <p>Kategoriler
+                    <span className='list'>{kategoriler.map(i => <em key={i} >{i} ,</em>)}</span> </p>
+                <label>
                     <span>Yazı İçerik</span>
-                    <textarea onChange={(e)=>setIcerik(e.target.value)} value={icerik} required></textarea>
+                    <textarea onChange={(e) => setIcerik(e.target.value)} value={icerik} required></textarea>
                 </label>
                 <label>
                     <span>Okunma Süresi</span>
-                    <input type="number" onChange={(e)=>setOkunmaSuresi(e.target.value)} value={okunmaSuresi} required />
+                    <input type="number" onChange={(e) => setOkunmaSuresi(e.target.value)} value={okunmaSuresi} required />
                 </label>
                 <button className='btn' >Oluştur</button>
             </form>
