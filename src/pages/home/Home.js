7c830e08bc036613674458d62644c2d189d81dab
Engin ERZİK <erzik.engin@gmail.com>
2023-06-15 13:43:13 +0300

    Verileri Gösterme İşlemi

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,19 @@
 import './Home.css'
+import { useFetch } from '../../hooks/useFetch'
 
+import './Home.css'
+import Blog from '../blog/Blog';
 
 export default function Home() {
+    const {data,yukleniyor,hata}=useFetch('http://localhost:8000/bloglar');
+   // console.log(data);
     return (
-        <div>
-            <h1>  Home</h1>
+        <div className='home'>
+           {hata&&<p className='error' >{hata}</p>}
+           {yukleniyor&&<p>Yükleniyor...</p>}
+           {data && data.map(blog=>(
+            <h2 key={blog.id} >{blog.baslik}</h2>
+           ))}
         </div>
     )
 }
