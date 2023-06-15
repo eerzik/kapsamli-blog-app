b37d5e155c2d9398e87d0d48a3ae839393db8647
Engin ERZİK <erzik.engin@gmail.com>
2023-06-15 16:48:30 +0300

    Arama Componetini Oluşturma

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import Searchbar from './Searchbar'
 export default function Navbar() {
     return (
         <div className='navbar' >
             <nav>
                 <Link to='/' className="brand" > <h1>AOS Blog</h1> </Link>
+                <Searchbar />
                 <Link to='/create' > <h1>Yeni Yazı</h1> </Link>
             </nav>
         </div>
