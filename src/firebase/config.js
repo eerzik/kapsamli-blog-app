3098bea4d31962c3b3bc14fb969dc15dcee0e753
Engin ERZİK <erzik.engin@gmail.com>
2023-06-16 11:46:19 +0300

    FireBase bağlantısını oluşturma npm install firebase@9 Firestore Database collection oluştur. +Add app oluştur. app den config bilgilerini al

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import{initializeApp} from 'firebase/app'
+import{getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBY_7A9MZeGJ_x68JQL3JGX_lVbzDQliWw",
+    authDomain: "modern-react-app-af090.firebaseapp.com",
+    projectId: "modern-react-app-af090",
+    storageBucket: "modern-react-app-af090.appspot.com",
+    messagingSenderId: "824161797109",
+    appId: "1:824161797109:web:980c05f198cc63854996bc"
+};
+
+initializeApp(firebaseConfig)
+
+const db=getFirestore();
+export {db};
