f23c9e8653120b81caa3440f4a589bcd6b5a0382
Engin ERZİK <erzik.engin@gmail.com>
2023-06-16 10:35:39 +0300

    Reducer Oluşturma

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ import { useTheme } from '../hooks/useTheme'
 
 export default function Navbar() {
 
-    const {bgColor}=useTheme();
-
+    const { bgColor, changeColor } = useTheme();
+    console.log(bgColor);
     return (
         //Navbar ın rengini themecontext den getirdik.
-        <div className='navbar' style={{backgroundColor:bgColor}} >
-            <nav>
+        <div className='navbar' style={{ backgroundColor: bgColor }} >
+            <nav onClick={() =>  changeColor('#c44569') } >
                 <Link to='/' className="brand" > <h1>AOS Blog</h1> </Link>
                 <Searchbar />
                 <Link to='/create' > <h1>Yeni Yazı</h1> </Link>
