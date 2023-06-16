845812d4828a1314acedf6bbec24b00342029faf
Engin ERZİK <erzik.engin@gmail.com>
2023-06-16 14:58:39 +0300

    FireStore Veri Silme İşlemi OnSnapchat kullanmadık bunları ekleyebiliriz. olmadığı için sayfa yenilenmeden datalar gelmiyor

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,29 +1,44 @@
 import { useTheme } from '../hooks/useTheme'
 import './BlogList.css'
-import {Link} from 'react-router-dom'
-export default function BlogList({bloglar}) {
+import { Link } from 'react-router-dom'
+import DeleteIcon from '../assets/delete.svg'
+// import { useEffect, useState } from 'react';
+import { db } from '../firebase/config'
+import { doc, deleteDoc } from 'firebase/firestore'
 
-    const {mode}=useTheme();
-    
-    if(bloglar.length===0)
-    {
+
+
+export default function BlogList({ bloglar }) {
+
+    const { mode } = useTheme();
+
+    if (bloglar.length === 0) {
         return <div className='error' >Aranan Yazı Bulunamadı!</div>
     }
+
+    const handleDelelete = async (id) => {
+        console.log(id)
+
+        const ref=doc(db,'bloglar',id)
+        await deleteDoc(ref);
+    }
+
 
     return (
         <div className='blog-list'>
             {
-                bloglar.map(blog=>(
+                bloglar.map(blog => (
                     <div key={blog.id} className={`card ${mode}`} >
-                         <h3  >{blog.baslik}</h3>
-                         <p>{blog.okunmaSuresi}</p>
-                         <div>
-                            {blog.icerik.substring(0,100) }...
-                         </div>
-                         <Link to={`/blog/${blog.id}`} > Daha Fazla Oku </Link>
+                        <h3  >{blog.baslik}</h3>
+                        <p>{blog.okunmaSuresi}</p>
+                        <div>
+                            {blog.icerik.substring(0, 100)}...
+                        </div>
+                        <Link to={`/blog/${blog.id}`} > Daha Fazla Oku </Link>
+                        <img className='delete' onClick={() => handleDelelete(blog.id)} src={DeleteIcon} alt='Yazı Sil' ></img>
                     </div>
-                   
-                   ))
+
+                ))
             }
         </div>
     )
